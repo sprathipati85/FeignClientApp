33ca161057d48b999598a95d947681bb39988fc1
sprathipati <sprathipati85@gmail.com>
2017-02-07 22:11:41 +0000

    first commit

---
+++ src/main/java/com/jpmorgan/spred/ProductController.java
@@ -0,0 +1,35 @@
+package com.jpmorgan.spred;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="client/products")
+public class ProductController {
+
+	@Autowired 
+	ProductClient client;
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public List<Product> list(){
+		System.out.println("product client Application");
+
+		return client.all();
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public Product getById(@PathVariable("id") Long id){
+		return client.getById(id);
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public void save(@RequestBody Product product){
+		client.add(product);;
+	}	
+}
